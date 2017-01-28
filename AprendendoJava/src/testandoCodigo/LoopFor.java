7adee62532558336db0a2e4db1098f1a688edee3
Marcilene <Marcilene@Henrique-PC>
2017-01-28 15:13:04 -0200

    klklk

--- AprendendoJava/src/testandoCodigo/LoopFor.java
+++ AprendendoJava/src/testandoCodigo/LoopFor.java
@@ -7,11 +7,20 @@ import java.util.Scanner;
 public class LoopFor {
 	public static void main(String[] args) {
 		
-		int numero ;
+		int[] numero =new int[10] ;
 		Scanner dado = new Scanner(System.in);
 		//dado.numeronextInt() 
 		
 		System.out.println("Quantas vezes quer que repita o seu nome ?");
+		for (int i = 0; i < args.length; i++) {
+			if (i >= 10) {
+				
+				System.out.println("Obrigado");
+			}else{
+				System.out.println("Nao eh a quantidade permitida!");
+			}
+			
+		}
 		
 	}
 	
