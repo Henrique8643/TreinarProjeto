10c8a8fab318cab1d1889005ce7664b04d6ec127
Marcilene <Marcilene@Henrique-PC>
2017-01-29 23:59:13 -0200

    Outra forma de fazer uma contagem de multiplos.

--- projeto/Treinar/src/Treinando/ExercicioFor.java
+++ projeto/Treinar/src/Treinando/ExercicioFor.java
@@ -1,19 +1,22 @@
 package Treinando;
 
+import java.util.stream.IntStream;
+
 public class ExercicioFor {
 	public static void main(String[] args) {
-		int numero = 0;
-		int soma = 0 ;
+//		int numero = 0;
+//		int soma = 0 ;
 //		Somando de 1 até 1500.
-		for (int i = 1; i < 1500; i++) {
+//		for (int i = 1; i < 1500; i++) {
+		//Outra forma de fazer um contador 	
+		
+//			numero ++; 
+//			soma = soma + numero;
 			
-			numero ++; 
-			soma = soma + numero;
-			
-			System.out.println("Numero atual da contagem : " + numero + " Soma :" + soma);
+			//System.out.println("Numero atual da contagem : " + numero + " Soma :" + soma);
 			
 			
 		}
 	}
 
-}
+//}
--- projeto/Treinar/src/Treinando/ExerciciosMultiplo.java
+++ projeto/Treinar/src/Treinando/ExerciciosMultiplo.java
@@ -1,13 +1,18 @@
 package Treinando;
 
+//import java.util.stream.IntStream;
+
 public class ExerciciosMultiplo {
 	public static void main(String[] args) {
 		
-		
+//		IntStream.range(1, 100)
+//		.filter(numero -> (numero % 3 == 0)/*|| ( numero % 5 == 0 )*/)// poderia fazer de 5. 
+//		.forEach(numero -> System.out.println(numero));
+//		System.out.println(num + " eh multiplo de 3 !");
 		for (int i = 1; i < 100; i++) {
 			
-			if (i % 3 == 1) {
-				System.out.println(i + " eh multiplo de 3 !");
+			if (i % 3 == 0) {
+				System.out.println(i);
 			}
 		}
 	}
